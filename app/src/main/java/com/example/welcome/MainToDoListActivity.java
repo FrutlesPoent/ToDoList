62e7c8053ea20b13643cb4502e1c079d1dcf41a2
FrutlesPoent <frutlespoent@gmail.com>
2021-03-07 16:26:01 +0700

    Add first activity

---
+++ app/src/main/java/com/example/welcome/MainToDoListActivity.java
@@ -0,0 +1,14 @@
+package com.example.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainToDoListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_to_do_list);
+    }
+}
