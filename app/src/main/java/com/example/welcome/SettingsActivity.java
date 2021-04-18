e833080aba594c8a1f6393eb74f494387870dcb7
FrutlesPoent <frutlespoent@gmail.com>
2021-04-19 03:29:20 +0700

    add photo

--- app/src/main/java/com/example/welcome/SettingsActivity.java
+++ app/src/main/java/com/example/welcome/SettingsActivity.java
@@ -1,17 +1,22 @@
 package com.example.welcome;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private ImageView imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,34 @@ public class SettingsActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView2);
         imageView.setImageResource(R.drawable.icon);
+
+
+    }
+
+    public void onClickChangeAvatarButton(View view) {
+        Button button = view.findViewById(R.id.change_avatar_button);
+        imageView2 = view.findViewById(R.id.imageView2);
+        getImage();
+
+    }
+
+
+    private void getImage() {
+        Intent intentChooser = new Intent();
+        intentChooser.setType("image/*");
+        intentChooser.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentChooser, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && data != null && data.getData() != null){
+            if(resultCode == RESULT_OK){
+                imageView2.setImageURI(data.getData());
+
+            }
+        }
     }
 }
