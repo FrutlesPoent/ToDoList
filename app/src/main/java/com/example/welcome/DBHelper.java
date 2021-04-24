63ed1509c820fedecd5fea1bd7a2fe5cd2384207
FrutlesPoent <frutlespoent@gmail.com>
2021-04-25 01:02:47 +0700

    Add 3 table

--- app/src/main/java/com/example/welcome/DBHelper.java
+++ app/src/main/java/com/example/welcome/DBHelper.java
@@ -11,12 +11,15 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1; // for check if database is old
     public static final String DATABASE_NAME = "taskDb";
     public static final String TABLE_TASKS = "contacts";
+    public static final String TABLE_COMPLETED = "completed";
+    public static final String TABLE_IMAGE = "Image";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_TASK = "task"; // our task
     public static final String KEY_TIME = "time"; // time left
     public static final String KEY_PRIORITY = "priority"; // task priority
     public static final String KEY_COMPLETED = "completed";
+    public static final String KEY_IMAGE = "image";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME,null, DATABASE_VERSION);
@@ -26,7 +29,9 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_TASKS + "(" + KEY_ID + " integer primary key,"
-                + KEY_TASK + " text," + KEY_PRIORITY + " text," + KEY_TIME + " text," + KEY_COMPLETED + " integer" + ")");
+                + KEY_TASK + " text," + KEY_PRIORITY + " integer," + KEY_TIME + " text" + ")");
+        db.execSQL("create table " + TABLE_COMPLETED + "(" + KEY_ID + " integer primary key," + KEY_COMPLETED + " integer" + ")");
+        db.execSQL("create table " + TABLE_IMAGE + "(" + KEY_ID + " integer primary key," + KEY_IMAGE + " blob" + ")");
 
     }
 
