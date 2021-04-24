bb325b569f7e4c9a8c882deaa06c2fb5be385b57
FrutlesPoent <frutlespoent@gmail.com>
2021-04-25 01:51:02 +0700

    small changes

--- app/src/main/java/com/example/welcome/MainActivity.java
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, MainToDoListActivity.class);
+        startActivity(intent);
     }
 
     public void onClickStart(View view){
